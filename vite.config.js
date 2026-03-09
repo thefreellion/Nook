39da41409fdbc0576912f7a4bcb9e9d981ddd2d5
abi <freellion@gmail.com>
2026-03-09 16:15:52 -0700

    remove cloudflare and Base44 dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import base44 from "@base44/vite-plugin"
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+// import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
 });
